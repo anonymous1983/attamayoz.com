e9fe93ade8d4c0d4b16aa9e830dc88f450ceb784
Abid ANIS <anab.tn@gmail.com>
2014-01-26 13:16:25 +0100

    update path 20140126

--- patch/2010126/init.php
+++ patch/2010126/init.php
@@ -7,6 +7,6 @@
 $sql = "
         UPDATE att_db_tree AS T SET cost = ( SELECT cost
         FROM att_db_tree_type AS TT
-        WHERE TT.id_tree_type = t.id_tree_type ) 
+        WHERE TT.id_tree_type = T.id_tree_type ) 
         WHERE 1
         ";
